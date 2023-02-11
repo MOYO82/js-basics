327061ed3f31c33c899bb9b3f357e13d06389351
motunrayosholola <motunrayosholola@gmail.com>
2023-02-11 04:27:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Stutern Cohort 1.4');
+
+//function to multiply two numbers
+function multiplyNumbers(number1, number2) {
+    return number1*number2;
+}
+
+console.log(multiplyNumbers(10, 13))
